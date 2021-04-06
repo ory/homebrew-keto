919249ff8f479a7eeff2414b137f79cbcf0c5bbc
aeneasr <3372410+aeneasr@users.noreply.github.com>
2021-04-06 15:18:28 +0200

    Brew formula update for keto version v0.6.0-alpha.1.pre.1

--- keto.rb
+++ keto.rb
@@ -5,16 +5,16 @@
 class Keto < Formula
   desc ""
   homepage "https://www.ory.sh"
-  version "0.6.0-alpha.1.pre.0"
+  version "0.6.0-alpha.1.pre.1"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ory/keto/releases/download/v0.6.0-alpha.1.pre.0/keto_0.6.0-alpha.1.pre.0-sqlite_macos_64bit.tar.gz"
-    sha256 "f75a35654c08fc81bd68f3ee5cc36e1cc8e810713756f6c68c38add9d789f444"
+    url "https://github.com/ory/keto/releases/download/v0.6.0-alpha.1.pre.1/keto_0.6.0-alpha.1.pre.1-sqlite_macos_64bit.tar.gz"
+    sha256 "4380b1f356e3ec372a46ecee5dadd20cb5829665b2446b78489b431c5194ec75"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ory/keto/releases/download/v0.6.0-alpha.1.pre.0/keto_0.6.0-alpha.1.pre.0-sqlite_linux_64bit.tar.gz"
-    sha256 "78a81e99005c430f1a502849b1ff3f9509659d1994c7ada66347284929a5143b"
+    url "https://github.com/ory/keto/releases/download/v0.6.0-alpha.1.pre.1/keto_0.6.0-alpha.1.pre.1-sqlite_linux_64bit.tar.gz"
+    sha256 "c8d973be197a45e2d812afac45c3af5e22197f41d86a280c90f041d4641f348f"
   end
 
   def install
